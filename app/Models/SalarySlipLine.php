2a28d9e43e5dc073cea5dd6d0aa1597405890a26
WellkitDev <rahmatpurnomo808@gmail.com>
2026-07-22 02:36:15 +0700

    refactor(slip-gaji): rename migrasi hindari tabrakan timestamp + buang HasFactory tak terpakai
    Co-Authored-By: Mira <admin@avidpedia.com>

--- app/Models/SalarySlipLine.php
+++ app/Models/SalarySlipLine.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SalarySlipLine extends Model
 {
-    use HasFactory;
-
     protected $table = 'tb_salary_slip_lines';
 
     protected $fillable = ['salary_slip_id', 'type', 'label', 'amount', 'position'];
